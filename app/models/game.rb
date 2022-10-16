0f1a65dc11503c4469e62da0423b8175e6207e85
Anna Schmidt <marshall1697@gmail.com>
2022-10-15 19:11:56 -0600

    commit tree 259a31c04e5f434bb33a9499df932840b868c7af

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
-  
+
+    # def reviews
+    #     Review.where(game_id: self.id)
+    #   end
+    #instead do this built in method!lea
+
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   
+    # def game
+    #     Game.find(self.game_id)
+    # end
+    #instead do this!
+    belongs_to :game
 end
